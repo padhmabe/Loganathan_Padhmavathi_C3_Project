704b603ff733df95ae045819dec18d7b3705002b
Padhmavathi Loganathan <33409@ITCINFOTECH.COM>
2022-01-05 16:06:40 +0530

    Part 2:Solution

---
+++ Restaurant_project -Student version/src/main/java/Restaurant.java
@@ -0,0 +1,81 @@
+import com.sun.xml.internal.ws.api.pipe.ServerTubeAssemblerContext;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+public class Restaurant {
+    private String name;
+    private String location;
+    public LocalTime openingTime;
+    public LocalTime closingTime;
+    private List<Item> menu = new ArrayList<Item>();
+
+    public Restaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        this.name = name;
+        this.location = location;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+    }
+
+    public boolean isRestaurantOpen() {
+        boolean isRestaurantOpen ;
+
+        isRestaurantOpen =  (getCurrentTime().isAfter(openingTime)) && (getCurrentTime().isBefore(closingTime));
+
+
+        return isRestaurantOpen;
+    }
+    public LocalTime getCurrentTime(){ return  LocalTime.now(Clock.systemDefaultZone()); }
+
+    public List<Item> getMenu() {
+        return  Collections.unmodifiableList(menu);
+    } //All methods implemented
+
+    private Item findItemByName(String itemName){
+        for(Item item: menu) {
+            if(item.getName().equals(itemName))
+                return item;
+        }
+        return null;
+    }
+
+    public void addToMenu(String name, int price) {
+        Item newItem = new Item(name,price);
+        menu.add(newItem);
+    }
+    
+    public void removeFromMenu(String itemName) throws itemNotFoundException {
+
+        Item itemToBeRemoved = findItemByName(itemName);
+        if (itemToBeRemoved == null)
+            throw new itemNotFoundException(itemName);
+
+        menu.remove(itemToBeRemoved);
+    }
+    public void displayDetails(){
+        System.out.println("Restaurant:"+ name + "\n"
+                +"Location:"+ location + "\n"
+                +"Opening time:"+ openingTime +"\n"
+                +"Closing time:"+ closingTime +"\n"
+                +"Menu:"+"\n"+getMenu());
+
+    }
+
+    public String getName() {
+        return name;
+    }
+    public LocalTime getClosingTime()
+    {
+        return  closingTime;
+    }
+    public LocalTime getOpeningTime()
+    {
+        return  openingTime;
+    }
+
+}
